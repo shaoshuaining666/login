46ba05973ff1a2aa5e99b997d11ed83d2e6b5f73
shaoshuaining <1395312845@qq.com>
2021-01-12 17:50:08 +0800

    启动类加入自动扫描mapper文件注解

--- src/main/java/com/hotlel/demo/DemoApplication.java
+++ src/main/java/com/hotlel/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.hotlel.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hotlel.demo.mapper")
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class DemoApplication {
     }
 
 }
+
