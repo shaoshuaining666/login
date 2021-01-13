14564dc53962fcd83d9e5c53cfa1eba3b836069a
shaoshuaining <1395312845@qq.com>
2021-01-13 11:28:35 +0800

    加入jwt

--- src/main/java/com/hotlel/demo/mapper/UserMapper.java
+++ src/main/java/com/hotlel/demo/mapper/UserMapper.java
@@ -4,9 +4,6 @@ package com.hotlel.demo.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.hotlel.demo.bean.entity.UserDO;
 
-/**
- * Create By LB on 2020/5/28.
- */
 public interface UserMapper extends BaseMapper<UserDO> {
 
 }
