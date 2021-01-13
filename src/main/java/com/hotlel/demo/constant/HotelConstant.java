539e9fd8fb10806067ddf048c5dc8d3fc226edd0
Administrator <1542997738@qq.com>
2021-01-13 16:58:27 +0800

    常量

---
+++ src/main/java/com/hotlel/demo/constant/HotelConstant.java
@@ -0,0 +1,20 @@
+package com.hotlel.demo.constant;
+
+public interface HotelConstant {
+    String HASH_ALGORITHM_NAME = "md5";
+
+    int HASH_ITERATIONS = 1024;
+
+    String INFO = "info";
+
+    String ERROR_PAGE = "error";
+
+    String SUCCESS_PAGE = "success";
+    int MAX_UPLOAD_SIZE = 5242880;
+
+    int APK_UPLOAD_SIZE = 5242880;
+
+    String BACKEND_MODULE = "backendModule";
+
+    String DEV_MODULE = "devModule";
+}
