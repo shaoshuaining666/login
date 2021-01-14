f0989a6bc2b19a2d1527f97e8bb01b0ac1d35cf5
shaoshuaining <1395312845@qq.com>
2021-01-14 10:05:34 +0800

    添加文件上传oss接口

--- src/main/java/com/hotlel/demo/controller/DetaitMsgController.java
+++ src/main/java/com/hotlel/demo/controller/DetaitMsgController.java
@@ -3,10 +3,16 @@ package com.hotlel.demo.controller;
 import com.hotlel.demo.bean.common.JsonData;
 import com.hotlel.demo.bean.entity.DetaitMsg;
 import com.hotlel.demo.service.DetaitMsgService;
+import com.hotlel.demo.util.FileReadName;
+import com.hotlel.demo.util.FilesUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 @RequestMapping("detaitmsg")
@@ -38,5 +44,22 @@ public class DetaitMsgController {
     public JsonData updateDetaitMsg(DetaitMsg detaitMsg){
         detaitMsgService.updateDetaitMsg(detaitMsg);
         return JsonData.success();
+    }
+
+    @RequestMapping("uploadFile")
+    public JsonData uploadFile(@RequestBody MultipartFile multipartFile){
+        File file = FileReadName.readFiles(multipartFile);
+//        String lastName = file.getName().substring(file.getName().lastIndexOf("."));
+//        String fileName = UUID.randomUUID().toString() + lastName;
+        StringBuffer url = new StringBuffer();
+
+        try {
+            String s = FilesUtils.fileUpload(file);
+            url.append(s);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(url);
+        return JsonData.success(url);
     }
 }
